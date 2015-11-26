2d72bbdde1b36a3cddd4b3ce70f3413960305042
dermersean <dermersean@gmail.com>
2015-11-26 23:27:49 +0200

    html,css,js for food result page

--- static/js/mainJs.js
+++ static/js/mainJs.js
@@ -1,9 +1,25 @@
 /**
  * Created by dermersean on 26/11/2015.
  */
-$('a').click(function(){
+
+$('#suggestions').click(function(){
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
     return false;
 });
+
+$('#submitSearch').click(function()
+{
+    var searchWord = $('#searchFood').val();
+    $.post('/commitSearch',searchWord);
+});
+
+$("#img1").hover(function(){},function(){suggestions()});
+
+function suggestions()
+{
+    document.getElementById("img1").style.display = "none";
+    document.getElementById("img5").style.display = "inline";
+    //document.getElementById("here").innerHTML=output;
+}
