b4761aedfda14001c56c9fc106c280465e148fab
omrikap <omrikap@gmail.com>
2015-11-26 19:06:01 +0200

    test movie images

--- deliciouserver.py
+++ deliciouserver.py
@@ -10,15 +10,15 @@ import urllib.request
 
 movies = [['Pulp Fiction', 'http://ia.media-imdb.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_SX214_AL_.jpg'],
           ['Good Will Hunting', 'http://ia.media-imdb.com/images/M/MV5BMTk0NjY0Mzg5MF5BMl5BanBnXkFtZTcwNzM1OTM2MQ@@._V1_SY317_CR1,0,214,317_AL_.jpg'],
-          ['The Big Lebowski', 'http://ia.media-imdb.com/images/M/MV5BMTQ0NjUzMDMyOF5BMl5BanBnXkFtZTgwODA1OTU0MDE@._V1_SX214_AL_.jpg'],
+          ['The Big Lebowski', 'http://www.movieposterdb.com/posters/09_02/1998/118715/l_118715_e3aa6190.jpg'],
           ['V for Vendetta', 'http://ia.media-imdb.com/images/M/MV5BOTI5ODc3NzExNV5BMl5BanBnXkFtZTcwNzYxNzQzMw@@._V1_SY317_CR0,0,214,317_AL_.jpg'],
           ['Fight Club', 'http://ia.media-imdb.com/images/M/MV5BMjIwNTYzMzE1M15BMl5BanBnXkFtZTcwOTE5Mzg3OA@@._V1_SX214_AL_.jpg'],
-          ['Gladiator', 'http://ia.media-imdb.com/images/M/MV5BMjA4ODQ3ODkzNV5BMl5BanBnXkFtZTYwOTc4NDI3._V1_SX214_AL_.jpg'],
+          ['Gladiator', 'http://www.movieposterdb.com/posters/08_08/2000/172495/l_172495_2cce6a7c.jpg'],
           ['Interstellar', 'http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX214_AL_.jpg'],
-          ['Into the Wild', 'http://ia.media-imdb.com/images/M/MV5BMTAwNDEyODU1MjheQTJeQWpwZ15BbWU2MDc3NDQwNw@@._V1_SY317_CR0,0,214,317_AL_.jpg'],
+          ['Into the Wild', 'http://www.movieposterdb.com/posters/08_03/2007/758758/l_758758_cf9ee8b4.jpg'],
           ['The Imitation Game', 'http://ia.media-imdb.com/images/M/MV5BNDkwNTEyMzkzNl5BMl5BanBnXkFtZTgwNTAwNzk3MjE@._V1_SY317_CR0,0,214,317_AL_.jpg'],
           ['The Truman Show', 'http://ia.media-imdb.com/images/M/MV5BMTg4NTU3NTAyMF5BMl5BanBnXkFtZTgwNjYwNzc3NjE@._V1_SX214_AL_.jpg'],
-          ['Toy Story 3', 'http://ia.media-imdb.com/images/M/MV5BMTgxOTY4Mjc0MF5BMl5BanBnXkFtZTcwNTA4MDQyMw@@._V1_SY317_CR5,0,214,317_AL_.jpg'],
+          ['Toy Story 3', 'http://www.movieposterdb.com/posters/10_04/2010/435761/l_435761_1e6b9c8e.jpg'],
           ['Shawshank Redemption', 'http://ia.media-imdb.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SX214_AL_.jpg'],
           ['Forrest Gump', 'http://ia.media-imdb.com/images/M/MV5BMTQwMTA5MzI1MF5BMl5BanBnXkFtZTcwMzY5Mzg3OA@@._V1_SX214_AL_.jpg'],
           ['Matrix', 'http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX214_AL_.jpg'],
@@ -26,7 +26,7 @@ movies = [['Pulp Fiction', 'http://ia.media-imdb.com/images/M/MV5BMTkxMTA5OTAzMl
           ['Inside Out', 'http://ia.media-imdb.com/images/M/MV5BOTgxMDQwMDk0OF5BMl5BanBnXkFtZTgwNjU5OTg2NDE@._V1_SX214_AL_.jpg'],
           ['Snatch', 'http://ia.media-imdb.com/images/M/MV5BMTk5NzE0MDQyNl5BMl5BanBnXkFtZTcwNzk4Mjk3OA@@._V1_SY317_CR2,0,214,317_AL_.jpg'],
           ['The Good, the Bad and the Ugly', 'http://ia.media-imdb.com/images/M/MV5BOTQ5NDI3MTI4MF5BMl5BanBnXkFtZTgwNDQ4ODE5MDE@._V1_SX214_AL_.jpg'],
-          ['Goodfellas', 'http://ia.media-imdb.com/images/M/MV5BMTY2OTE5MzQ3MV5BMl5BanBnXkFtZTgwMTY2NTYxMTE@._V1_SX214_AL_.jpg'],
+          ['Goodfellas', 'http://www.movieposterdb.com/posters/05_09/1990/0099685/l_50996_0099685_be22f728.jpg'],
           ['City of God', 'http://ia.media-imdb.com/images/M/MV5BMjA4ODQ3ODkzNV5BMl5BanBnXkFtZTYwOTc4NDI3._V1_SX214_AL_.jpg']]
 
 
@@ -66,9 +66,9 @@ class getInfo(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         (movie_name, movie_image) = movies[random.randrange(20)]
         (food_name, food_image) = foods[random.randrange(20)]
-        new_path = "static/tmp/" + str(movie_name).strip().lower()
-        urllib.request.urlretrieve(movie_image, new_path)
-        json_dict = {"movieId": random.randrange(5), "movieName": movie_name, "movieImage": new_path,
+        # new_path = "static/tmp/" + str(movie_name).strip().lower() # todo remove
+        # urllib.request.urlretrieve(movie_image, new_path) # todo remove
+        json_dict = {"movieId": random.randrange(5), "movieName": movie_name, "movieImage": movie_image,
                      "recipeId": random.randrange(5), "recipeName": food_name, "recipeImage": food_image}
         self.finish(json.dump(json_dict, self))
 
